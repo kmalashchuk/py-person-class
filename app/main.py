1c8373505387de520426eb60dc7246ca7f24fa34
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2024-12-29 17:31:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+
+        self.name = name
+        self.age = age
+        self.spouse = None
+        Person.people[name] = self
+
+    def __repr__(self):
+
+        spouse_info = f", Spouse: {self.spouse.name}" if self.spouse else ""
+        return f"Person(Name: {self.name}, Age: {self.age}{spouse_info})"
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_data):
+    for person in people_data:
+        name = person["name"]
+        age = person["age"]
+        Person(name, age)
+
+    for person in people_data:
+        name = person["name"]
+        spouse_name = person.get("wife") or person.get("husband")
+        if spouse_name:
+            Person.people[name].spouse = Person.people.get(spouse_name)
+
+    return list(Person.people.values())
